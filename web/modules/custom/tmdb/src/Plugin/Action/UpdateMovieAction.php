4883c0bd2c80c55fdb2bbd896d676c277b061030
Jim Vomero <jim.vomero@fourkitchens.com>
2020-04-20 00:57:52 -0400

    feat06: Creating bulk action boillerplate.

---
+++ web/modules/custom/tmdb/src/Plugin/Action/UpdateMovieAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\tmdb\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Action description.
+ *
+ * @Action(
+ *   id = "update_movie_action",
+ *   label = @Translation("Update Movie"),
+ *   type = "node",
+ *   requirements = {
+ *     "_permission" = "edit any movie content",
+ *   },
+ * )
+ */
+class UpdateMovieAction extends ActionBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    // Do some processing..
+
+    // Don't return anything for a default completion message, otherwise return translatable markup.
+    return $this->t('Some result');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\Core\Entity\EntityInterface $object */
+    return $object->access('update', $account, $return_as_object);
+  }
+
+}
